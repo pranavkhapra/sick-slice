a99c58ed069044a75717d1794e3d077c72e90a0d
pranavkhapra <pranavkhapra23@gmail.com>
2021-03-09 14:55:46 +0530

    added some code

--- src/templates/Pizza.js
+++ src/templates/Pizza.js
@@ -1,23 +1,44 @@
 import React from 'react';
 import { graphql } from 'gatsby';
+import Img from 'gatsby-image';
+import styled from 'styled-components';
 
-function pizzaDynamicPage() {
-  return <div>Pizzaaas</div>;
+const PizzaGrid = styled.div`
+  display: grid;
+  grid-gap: 2rem;
+  grid-template-columns: repeat(auto-fill, minmax(400px, 1fr));
+`;
+function pizzaDynamicPage({ data }) {
+  const { pizza } = data;
+  console.log(pizza);
+  return (
+    <PizzaGrid>
+      <Img fluid={pizza.image.asset.fluid} />
+      <div>
+        <h2 className="mark">{pizza.name}</h2>
+        <ul>
+          {pizza.toppings.map((topping) => (
+            <li key={topping.id}>{topping.name}</li>
+          ))}
+        </ul>
+      </div>
+    </PizzaGrid>
+  );
 }
 
 export const query = graphql`
   query($slug: String!) {
-    pizzas: sanityPizza(slug: { current: { eq: $slug } }){
+    pizza: sanityPizza(slug: { current: { eq: $slug } }) {
       name
       id
-      image{
-        asset{
-          fluid(maxWidth:800){
-            ..GatsbySanityImageFluid
+      image {
+        asset {
+          fluid(maxWidth: 800) {
+            ...GatsbySanityImageFluid
           }
         }
       }
-      toppings{
+      toppings {
         name
         id
         vegetarian
